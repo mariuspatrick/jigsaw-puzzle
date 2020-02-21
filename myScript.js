bcaed5b473b628886bfcb78e2d553d23e18d94b9
mariuspatrick <marius.patrick72@gmail.com>
2020-02-21 16:01:55 +0100

    Dividing background image - WIP

---
+++ myScript.js
@@ -0,0 +1,33 @@
+// function swapTiles(firstCell, secondCell) {
+//   let temporaryCell = document.getElementById(firstCell).className;
+//   //   let cellOne = document.getElementById(firstCell);
+//   let cellTwo = document.getElementById(secondCell).className;
+
+//   document.getElementById(firstCell).className = cellTwo;
+//   cellTwo = temporaryCell;
+// }
+
+// function myMove() {
+//   let cell = document.getElementById("tile-4-15");
+//   let position = 0;
+//   let id = setInterval(move, 5);
+
+//   function move() {
+//     if (position == 190) {
+//       clearInterval(id);
+//     } else {
+//       position++;
+//       //   console.log("position", position);
+//       console.log("cell: ", cell);
+//       cell.style.left = position + "px";
+//       console.log("cell", cell.style);
+//       //   console.log("style", cell.style.left);
+//     }
+//   }
+// }
+
+function myMove() {
+  console.log("this:", document.getElementsByClassName("white-cell"));
+
+  //   console.log(x);
+}
