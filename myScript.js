4b2b55364f3fa20cdc8d6f0247875a100dfa9de4
mariuspatrick <marius.patrick72@gmail.com>
2020-02-25 13:35:26 +0100

    Finished win condition method

--- myScript.js
+++ myScript.js
@@ -5,10 +5,39 @@ getElement = element => {
 tileIsWhite = (row, column) => {
   if (getElement(`cell-${row}-${column}`).className == "tile16") {
     return true;
-  } else {
-    console.log("Error! Click a different tile");
   }
 };
+
+didWin = (row, column) => {
+  let loopCount = 0;
+  let tilesNotWrong = 0;
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < column; j++) {
+      loopCount += 1;
+      if (
+        document.getElementsByClassName("main-table")[0].children[i].children[j]
+          .className == `tile${loopCount}`
+      ) {
+        tilesNotWrong += 1;
+        console.log("tilesNotWrong: ", tilesNotWrong);
+      }
+    }
+    if (tilesNotWrong === 16) {
+      console.log("You win");
+      return;
+    }
+  }
+};
+
+// didWin = () => {
+//   console.log(
+//     "table",
+//     document.getElementsByClassName("main-table")[0].children[2].children[3]
+//       .className
+//   );
+//   // console.log("children", document.body.children.length);
+// };
 
 swapTiles = (firstCell, secondCell) => {
   //Swaps tiles if second one is white
@@ -23,6 +52,7 @@ moveTile = (row, column) => {
   if (column < 4) {
     if (tileIsWhite(row, column + 1)) {
       swapTiles(`cell-${row}-${column}`, `cell-${row}-${column + 1}`);
+      didWin(row, column);
       return;
     }
   }
@@ -30,6 +60,7 @@ moveTile = (row, column) => {
   if (column > 1) {
     if (tileIsWhite(row, column - 1)) {
       swapTiles(`cell-${row}-${column}`, `cell-${row}-${column - 1}`);
+      didWin(row, column);
       return;
     }
   }
@@ -37,6 +68,7 @@ moveTile = (row, column) => {
   if (row < 4) {
     if (tileIsWhite(row + 1, column)) {
       swapTiles(`cell-${row}-${column}`, `cell-${row + 1}-${column}`);
+      didWin(row, column);
       return;
     }
   }
@@ -44,6 +76,7 @@ moveTile = (row, column) => {
   if (row > 1) {
     if (tileIsWhite(row - 1, column)) {
       swapTiles(`cell-${row}-${column}`, `cell-${row - 1}-${column}`);
+      didWin(row, column);
       return;
     }
   }
