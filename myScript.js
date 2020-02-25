854b9ed58b28f7ec2baa46f74d4e1dc8e0d44981
mariuspatrick <marius.patrick72@gmail.com>
2020-02-25 14:47:54 +0100

    Finished game

--- myScript.js
+++ myScript.js
@@ -8,47 +8,58 @@ tileIsWhite = (row, column) => {
   }
 };
 
+shuffleTiles = () => {
+  for (let row = 1; row <= 4; row++) {
+    for (let column = 1; column <= 4; column++) {
+      let rowToSwap = Math.floor(Math.random() * 4 + 1);
+      let columnToSwap = Math.floor(Math.random() * 4 + 1);
+
+      swapTiles(`cell-${row}-${column}`, `cell-${rowToSwap}-${columnToSwap}`);
+    }
+  }
+};
+
 didWin = (row, column) => {
   let loopCount = 0;
   let tilesNotWrong = 0;
 
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < column; j++) {
+      // Loops through the table like a 2D array and checks if tiles are
+      // in their right position
+
       loopCount += 1;
       if (
+        // Looping through the children of main table
         document.getElementsByClassName("main-table")[0].children[i].children[j]
           .className == `tile${loopCount}`
       ) {
         tilesNotWrong += 1;
-        console.log("tilesNotWrong: ", tilesNotWrong);
       }
     }
     if (tilesNotWrong === 16) {
-      console.log("You win");
+      let h1 = document.createElement("H1");
+      let text = document.createTextNode("You are a monk");
+
+      h1.appendChild(text);
+      document.body.appendChild(h1);
+
       return;
     }
   }
 };
 
-// didWin = () => {
-//   console.log(
-//     "table",
-//     document.getElementsByClassName("main-table")[0].children[2].children[3]
-//       .className
-//   );
-//   // console.log("children", document.body.children.length);
-// };
-
 swapTiles = (firstCell, secondCell) => {
-  //Swaps tiles if second one is white
-  let temp = getElement(firstCell).className;
+  let temporaryCell = getElement(firstCell).className;
 
   getElement(firstCell).className = getElement(secondCell).className;
 
-  getElement(secondCell).className = temp;
+  getElement(secondCell).className = temporaryCell;
 };
 
 moveTile = (row, column) => {
+  //Checks up, down, left, right if there is a white tile, and moves there
+
   if (column < 4) {
     if (tileIsWhite(row, column + 1)) {
       swapTiles(`cell-${row}-${column}`, `cell-${row}-${column + 1}`);
